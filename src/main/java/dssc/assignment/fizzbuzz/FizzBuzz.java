8706ee116d36cd1f79c7955522ada6edbd812a67
marcosicklinger <sicklinger.marco@gmail.com>
2021-11-15 09:47:19 +0100

    extended fizzbuzz

--- src/main/java/dssc/assignment/fizzbuzz/FizzBuzz.java
+++ src/main/java/dssc/assignment/fizzbuzz/FizzBuzz.java
@@ -3,6 +3,28 @@ package dssc.assignment.fizzbuzz;
 public class FizzBuzz {
 
     public static void main(String... args) {
+
+        for (int i = 1; i < 106; i++) {
+            if (i % 7 == 0) {
+                if (i % 3 == 0) {
+                    if (i % 5 == 0) {
+                        System.out.println("FizzBuzzBang " + i);
+                    }
+                    else {
+                        System.out.println("FizzBang " + i);
+                    }
+                }
+                else if (i % 5 == 0){
+                    System.out.println("BuzzBang " + i);
+                }
+                else {
+                    System.out.println("Bang " + i);
+                }
+            }
+            else {
+                System.out.println(i);
+            }
+        }
         
     }
 
