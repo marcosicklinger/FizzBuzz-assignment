5f93bb32409e5d02e5e33339878c180e284e1f41
marcosicklinger <sicklinger.marco@gmail.com>
2021-11-19 17:36:22 +0100

    method 'converter' added

--- src/main/java/dssc/assignment/fizzbuzz/FizzBuzz.java
+++ src/main/java/dssc/assignment/fizzbuzz/FizzBuzz.java
@@ -5,27 +5,31 @@ public class FizzBuzz {
     public static void main(String... args) {
 
         for (int i = 1; i < 106; i++) {
-            if (i % 7 == 0) {
-                if (i % 3 == 0) {
-                    if (i % 5 == 0) {
-                        System.out.println("FizzBuzzBang");
-                    }
-                    else {
-                        System.out.println("FizzBang");
-                    }
-                }
-                else if (i % 5 == 0){
-                    System.out.println("BuzzBang");
-                }
-                else {
-                    System.out.println("Bang");
-                }
-            }
-            else {
-                System.out.println(i);
-            }
+            System.out.println(converter(i));
         }
         
+    }
+
+    static String converter (int i) {
+        if (i % 7 == 0) {
+            if (i % 3 == 0) {
+                if (i % 5 == 0) {
+                    return "FizzBuzzBang";
+                }
+                else {
+                    return "FizzBang";
+                }
+            }
+            else if (i % 5 == 0){
+                return "BuzzBang";
+            }
+            else {
+                return "Bang";
+            }
+        }
+        else {
+            return "" + i;
+        }
     }
 
 }
